81dba7bb2f2b447f4e3434fb59bcbf566f8be54c
Terry Chia <chiawz@hotmail.com>
2024-05-17 01:43:52 +0800

    first commit

---
+++ CertificatePinning/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  CertificatePinning
+//
+//  Created by Chia Wei Zheng Terry on 16/5/24.
+//
+
+import Foundation
---
+++ CertificatePinning/Response.swift
@@ -0,0 +1,8 @@
+//
+//  Response.swift
+//  CertificatePinning
+//
+//  Created by Chia Wei Zheng Terry on 17/5/24.
+//
+
+import Foundation
