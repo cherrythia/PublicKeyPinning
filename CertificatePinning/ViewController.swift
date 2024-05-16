d647479720a331b6bc7a3cae789c2dce4f6f9fe7
Terry Chia <chiawz@hotmail.com>
2024-05-16 22:51:47 +0800

    Initial Commit

---
+++ CertificatePinning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CertificatePinning
+//
+//  Created by Chia Wei Zheng Terry on 16/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
